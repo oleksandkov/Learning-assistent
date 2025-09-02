54250d8f877c3a2c707213f5a6e7cc19959f57da
oleskandkov <oleksandr.kov.dm@gmail.com>
2025-09-02 21:50:07 +0300

    Add C and C++ programs to count numbers with unique digits between 2000 and 3000

---
+++ labs/c/number2-12-my/lab12.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i, num = 0;
+    for (i = 2000; i >= 2000 && i <= 3000; i ++ ) {
+        int r1, r2, r3, r4;
+        r1 = i / 1000;
+        r2 = i /100 % 10;
+        r3 = i /10 % 10;
+        r4 = i % 10;
+        if (r1 != r2 && r1 != r3 && r1 != r4 && r2 != r3 && r2 != r4 && r3 != r4) {
+            num++;
+        }
+    }
+    cout << ("Кількість чисел у запису яких немає двх однакових цифр:") << num << "\n";
+    return 0;
+
+}
