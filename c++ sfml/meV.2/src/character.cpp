816fd3223270915c68902581f10893b7f6b302fb
oleksandkov <oleksandr.kov.dm@gmail.com>
2025-11-25 22:24:26 +0200

    Add C++ instructions for Gooffy and improve character logic formatting

--- c++ sfml/meV.2/src/character.cpp
+++ c++ sfml/meV.2/src/character.cpp
@@ -2,8 +2,8 @@
 
 Character::Character()
 {
-    totalFrames = 8; 
-    animationSpeed = 0.1f; 
+    totalFrames = 8;
+    animationSpeed = 0.1f;
     currentFrame = 0;
     speed = 200.f;
     isWalking = false;
@@ -40,14 +40,14 @@ void Character::update()
 {
     if (isWalking)
     {
-        if(sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
         {
-            setScale(-1.f, 1.f); 
+            setScale(-1.f, 1.f);
             setOrigin(frameSize.x, 0);
         }
-        else if(sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+        else if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
         {
-            setScale(1.f, 1.f); 
+            setScale(1.f, 1.f);
             setOrigin(0, 0);
         }
         setTexture(walkTexture);
@@ -69,21 +69,21 @@ void Character::moveCharacter()
     sf::Vector2f oldpos = getPosition();
     float deltaTime = clock.restart().asSeconds();
     sf::Vector2f movement(0.f, 0.f);
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
-        movement.y += speed * deltaTime;
+    // if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+    //     movement.y += speed * deltaTime;
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
         movement.x -= speed * deltaTime;
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
         movement.x += speed * deltaTime;
-    // Jumping
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) && onGround) {
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) && onGround)
+    {
         velocity.y = jumpVelocity;
         isJumping = true;
     }
     isWalking = (movement.x != 0.f);
     move(movement);
     initializeHitbox();
-    for (const auto& rect : collisionList)
+    for (const auto &rect : collisionList)
     {
         if (hitbox.getGlobalBounds().intersects(rect))
         {
@@ -100,31 +100,37 @@ void Character::addToCollisionList(sf::FloatRect rect)
 
 void Character::initializeHitbox()
 {
-   float scale = 0.38f; // Adjust this value (0.8 = 80% size)
+    float scale = 0.38f; // Adjust this value (0.8 = 80% size)
     sf::FloatRect bounds = getGlobalBounds();
     hitbox.setSize(sf::Vector2f(bounds.width * scale, bounds.height * scale - 15.f));
-    hitbox.setPosition(bounds.left + (bounds.width * (1 - scale) / 2), bounds.top + (((bounds.height * (1 - scale)) + 107.f)/ 2));
+    hitbox.setPosition(bounds.left + (bounds.width * (1 - scale) / 2), bounds.top + (((bounds.height * (1 - scale)) + 107.f) / 2));
     hitbox.setFillColor(sf::Color::Transparent);
     hitbox.setOutlineColor(sf::Color::Red);
     hitbox.setOutlineThickness(1.f);
 }
 
-void Character::characterLogic() {
+void Character::characterLogic()
+{
     float dt = 0.016f; // Approximate delta time for 60 FPS
 
     // Check if on ground (intersects any collision object)
     onGround = false;
-    for (const auto& rect : collisionList) {
-        if (hitbox.getGlobalBounds().intersects(rect)) {
+    for (const auto &rect : collisionList)
+    {
+        if (hitbox.getGlobalBounds().intersects(rect))
+        {
             onGround = true;
             break;
         }
     }
 
     // Apply gravity if not on ground
-    if (!onGround) {
+    if (!onGround)
+    {
         velocity.y += 980.f * dt; // Gravity acceleration
-    } else {
+    }
+    else
+    {
         velocity.y = 0.f;
     }
 
@@ -132,13 +138,25 @@ void Character::characterLogic() {
     move(0.f, velocity.y * dt);
 
     // Check for collision after movement with any object
-    for (const auto& rect : collisionList) {
-        if (hitbox.getGlobalBounds().intersects(rect)) {
+    for (const auto &rect : collisionList)
+    {
+        if (hitbox.getGlobalBounds().intersects(rect))
+        {
             setPosition(getPosition().x, rect.top - getGlobalBounds().height);
             velocity.y = 0.f;
             onGround = true;
             isJumping = false;
             break; // Stop at the first collision (highest object)
         }
+        if ( !onGround)
+        {   
+            oldpos = getPosition();
+            if( hitbox.getGlobalBounds().intersects(rect) && velocity.y < 0)
+            {   
+            hitbox.setPosition(getPosition().x, rect.top - rect.height);
+            velocity.y = 0.f;
+            break;
+            }
     }
+}
 }
