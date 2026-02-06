32b70d7b4fe2ceba304eaee16d9614e25cda0c20
Oleksandr Koval <oleksandr.kov.dm@gmail.com>
2026-02-06 11:15:01 +0200

    update

--- OOP/TRAINNING#1/Training2/src/Main.java
+++ OOP/TRAINNING#1/Training2/src/Main.java
@@ -1,5 +1,8 @@
-import java.util.Scanner;//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+/**
+ * click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+ */
 void main() {
     Scanner scanner = new Scanner(System.in);
     Random random;
