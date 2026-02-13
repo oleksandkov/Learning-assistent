c0a912eb3f464efe8314fb8f8952ec3ba4c41703
Oleksandr Koval <oleksandr.kov.dm@gmail.com>
2026-02-13 09:52:58 +0200

    Refactor Example class and update Main class to improve object initialization and output formatting

--- OOP/LECTURE2/src/Example.java
+++ OOP/LECTURE2/src/Example.java
@@ -1,6 +1,44 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Example {
     private String name;
     private Integer kurs;
     private Double seredniBal;
-    private Map<String, >
+    private Map<String, Double> zachotka;
+
+    public static double stupendiumPorig = 4.0;
+
+    public String getName() { //аксесор
+        return name;
+    }
+
+    public void setName(String Name) { // мутатор, setter
+        this.name = Name;
+    }
+
+    public Double getSeredniBal() { // getter
+        return seredniBal;
+    }
+
+    public Example(String name, Integer kurs) {
+        this.name = name;
+        this.kurs = kurs;
+
+        seredniBal = 0.0;
+        zachotka =new HashMap<>();
+
+        System.out.println("Constructor: Example(String name, Integer kurs) ");
+        System.out.println("Object created");
+        print();
+    }
+
+    public void print() {
+        System.out.println("Student: " +name+" Kurs: " + kurs +" Serednibal: " + seredniBal + " Zachotka" + zachotka + "Stupendium Porig: " + stupendiumPorig);
+
+    }
+
+    public Example() {
+        this("Nigger", 1);
+    }
 }
--- OOP/LECTURE2/src/Main.java
+++ OOP/LECTURE2/src/Main.java
@@ -2,6 +2,9 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
+        Example s1 = new Example("Trunmp", 2);
+        Example.stupendiumPorig = 4.5;
+        Example s2 = new Example("Barack", 4);
+        Example s3 = new Example();
     }
 }
